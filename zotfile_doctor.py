e70e9988be4baddd8c814ed8071cda4e46ff4994
Toni <toni.giorgino@gmail.com>
2019-09-18 11:33:35 +0200

    pep8

--- zotfile_doctor.py
+++ zotfile_doctor.py
@@ -12,7 +12,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-
 """Checks the consistency between the zotfile-managed directory and the database"""
 
 import sqlite3
@@ -22,13 +21,14 @@ import unicodedata
 
 
 def get_db_set(db):
-    conn=sqlite3.connect(db)
+    conn = sqlite3.connect(db)
 
-    db_c=conn.execute('select path from itemAttachments where path like "attachments:%"')
-    db_d=db_c.fetchall()
+    db_c = conn.execute(
+        'select path from itemAttachments where path like "attachments:%"')
+    db_d = db_c.fetchall()
 
-    db_l = [ x[0].replace('attachments:',"") for x in db_d]
-    db_l = [ unicodedata.normalize("NFD", x) for x in db_l]
+    db_l = [x[0].replace('attachments:', "") for x in db_d]
+    db_l = [unicodedata.normalize("NFD", x) for x in db_l]
     db_set = set(db_l)
     return db_set
 
@@ -36,25 +36,27 @@ def get_db_set(db):
 def get_dir_set(d):
     p = pathlib.Path(d)
     fg = p.rglob("*.pdf")
-    fs = [ str(x.relative_to(p)) for x in fg ]
-    fs = [ unicodedata.normalize("NFD", x) for x in fs]
+    fs = [str(x.relative_to(p)) for x in fg]
+    fs = [unicodedata.normalize("NFD", x) for x in fs]
     d_set = set(fs)
     return d_set
 
-    
+
 def main(db, d):
-    db_set=get_db_set(db)
-    dir_set=get_dir_set(d)
+    db_set = get_db_set(db)
+    dir_set = get_dir_set(d)
 
     db_not_dir = db_set.difference(dir_set)
     dir_not_db = dir_set.difference(db_set)
 
-    print(f"There were {len(db_not_dir)} files in DB but not in zotfile directory:")
+    print(
+        f"There were {len(db_not_dir)} files in DB but not in zotfile directory:")
     for f in sorted(db_not_dir):
         print("   " + f)
 
     print("\n")
-    print(f"There were {len(dir_not_db)} files in zotfile directory but not in DB:")
+    print(
+        f"There were {len(dir_not_db)} files in zotfile directory but not in DB:")
     for f in sorted(dir_not_db):
         print("   " + f)
 
@@ -66,7 +68,3 @@ if __name__ == "__main__":
 
     main(sys.argv[1],
          sys.argv[2])
-
-    
-
-    
